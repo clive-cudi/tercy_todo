ad623b7fcaba213bd170267980af0f49224b0379
clive-cudi <clivemaina41@gmail.com>
2022-06-17 10:49:02 +0300

    further-navbar-styling-plus-animation

--- client_tercy_todo/components/reusable/Navbar.tsx
+++ client_tercy_todo/components/reusable/Navbar.tsx
@@ -1,23 +1,46 @@
-import styles from "../../styles/components/reusable/navbar.module.css";
+import { useState } from "react";
+import styles from "../../styles/components/reusable/navbar.module.scss";
+import { BsHouseDoor, BsCheckCircle } from "react-icons/bs";
+import { FiCheckCircle, FiBell } from "react-icons/fi";
+import { HiMenuAlt1 } from "react-icons/hi";
+import { AiFillStar } from "react-icons/ai";
+import { GoAlert } from "react-icons/go";
+import { FaHamburger } from "react-icons/fa"
 
 interface NavBarProps {
     starredTasks?: []
     urgentTasks?: []
 }
 
+const iconStyles: React.CSSProperties = {
+    margin: "10px",
+    fontSize: "20px"
+}
+
 export function NavBar({starredTasks, urgentTasks}: NavBarProps): JSX.Element {
+    const [isCollapsed, setIsCollapsed] = useState<boolean>(false);
+
+    function toggleCollapse(): void{
+        if (isCollapsed == true){
+            setIsCollapsed(false);
+        } else {
+            setIsCollapsed(true);
+        }
+    }
+
     return (
-        <nav className={styles.nav}>
+        <nav className={`${styles.nav} ${isCollapsed ? styles.nav_collapsed: ''}`}>
             <div className={styles.nav_wrapper}>
                 <div className={styles.nav_logo}>
-                    <h1>MY TODO</h1>
+                    {isCollapsed == false && <h1 className={styles.nav_logo_title}>MY TODO</h1>}
+                    <span onClick={():void=>{toggleCollapse()}}>{isCollapsed ? <FaHamburger /> : <HiMenuAlt1 />}</span>
                 </div>
                 <ul>
-                    <li><button>Home</button></li>
-                    <li><button>My Tasks</button></li>
-                    <li><button>Inbox</button></li>
+                    <li><button><BsHouseDoor />{isCollapsed == false && <span>Home</span>}</button></li>
+                    <li><button><FiCheckCircle />{isCollapsed == false && <span>My Tasks</span>}</button></li>
+                    <li><button><FiBell />{isCollapsed == false && <span>Inbox</span>}</button></li>   
                 </ul>
-                <span>Starred Tasks</span>
+                <span className={styles.nav_mini_title}>{isCollapsed == false ? <span className={styles.nav_mini_title_txt}>Starred Tasks</span> : ''}<AiFillStar /></span>
                 <ul>
                     {
                         starredTasks?.map((task)=>{
@@ -27,7 +50,7 @@ export function NavBar({starredTasks, urgentTasks}: NavBarProps): JSX.Element {
                         })
                     }
                 </ul>
-                <span>Urgent Tasks</span>
+                <span className={styles.nav_mini_title}>{isCollapsed == false ? <span className={styles.nav_mini_title_txt}>Urgent Tasks</span> : ''}<GoAlert /></span>
                 <ul>
                     {
                         urgentTasks?.map((task)=>{
